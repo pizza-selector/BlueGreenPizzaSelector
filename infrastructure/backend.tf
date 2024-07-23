aa79ee12e4cd40025f9dba5fe9e06353bc2af5f3
Mikhail Koptelov <mkoptelov@gmail.com>
2024-07-23 22:00:17 +0200

    Added backend service, updated apply stage

---
+++ infrastructure/backend.tf
@@ -0,0 +1,18 @@
+resource "azurerm_service_plan" "app_service_plan_be" {
+  name                = "pizza-selector-be-asp-${var.env}"
+  resource_group_name = azurerm_resource_group.pizza_selector_rg.name
+  location            = azurerm_resource_group.pizza_selector_rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "pizza_selector_be" {
+  name                = "pizza-selector-be-${var.env}"
+  resource_group_name = azurerm_resource_group.pizza_selector_rg.name
+  location            = azurerm_service_plan.app_service_plan_be.location
+  service_plan_id     = azurerm_service_plan.app_service_plan_be.id
+
+  site_config {
+    always_on = false
+  }
+}
