271e4673b263590b830c555cb512d8250519965e
Mikhail Koptelov <mkoptelov@gmail.com>
2024-08-01 20:31:37 +0200

    added depensd on?

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -33,6 +33,8 @@ resource "azurerm_mssql_firewall_rule" "azure_firewall_exception" {
   server_id        = azurerm_mssql_server.sql_server.id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "0.0.0.0"
+
+  depends_on = [azurerm_mssql_database.db]
 }
 
 resource "null_resource" "create-sql-user" {
