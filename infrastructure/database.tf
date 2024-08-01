13cbb6e123954d4c24c1dbf35dcf472fa34627b2
Mikhail Koptelov <mkoptelov@gmail.com>
2024-08-01 21:46:14 +0200

    connection string fixes

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -47,7 +47,7 @@ resource "null_resource" "create-sql-user" {
   provisioner "local-exec" {
     command = <<EOT
     Install-Module -Name SqlServer -Force
-     Invoke-Sqlcmd -Username "terraformoperator" -Password "${random_password.password.result}" -Query "CREATE USER [${azurerm_linux_web_app.pizza_selector_be.name}] FROM EXTERNAL PROVIDER; ALTER ROLE db_datareader ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}]; ALTER ROLE db_datawriter ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}]; ALTER ROLE db_ddladmin ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}];" -ConnectionString "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name}.database.windows.net,1433;Initial Catalog=Master;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;" 
+     Invoke-Sqlcmd -Query "CREATE USER [${azurerm_linux_web_app.pizza_selector_be.name}] FROM EXTERNAL PROVIDER; ALTER ROLE db_datareader ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}]; ALTER ROLE db_datawriter ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}]; ALTER ROLE db_ddladmin ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}];" -ConnectionString "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name},1433;Initial Catalog=Master;User Id=terraformoperator;Password=${random_password.password.result};Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;" 
     EOT
 
     interpreter = ["pwsh", "-Command"]
