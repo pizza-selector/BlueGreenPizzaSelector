6a676bb9f77c1f031045656e187a8eb992d51781
Mikhail Koptelov <mkoptelov@gmail.com>
2024-08-01 22:08:53 +0200

    separate sql step

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -1,26 +1,15 @@
-resource "random_password" "password" {
-  length      = 16
-  special     = true
-  min_lower   = 1
-  min_upper   = 1
-  min_numeric = 1
-  min_special = 1
-
-  override_special = "!#$%"
-}
 
 resource "azurerm_mssql_server" "sql_server" {
   name                         = "pizza-sqlserver-${var.env}"
   resource_group_name          = azurerm_resource_group.pizza_selector_rg.name
   location                     = azurerm_resource_group.pizza_selector_rg.location
   version                      = "12.0"
-  administrator_login          = "terraformoperator"
-  administrator_login_password = random_password.password.result
   minimum_tls_version          = "1.2"
 
   azuread_administrator {
+    azuread_authentication_only = true
     login_username = "Pizza Selector"
-    object_id      = "b05c30c9-c432-4c8d-ae49-1f4528f6ccdc"
+    object_id      = "dd8e1ef7-e75d-4373-868a-1f331442ba81"
   }
 }
 
@@ -31,27 +20,4 @@ resource "azurerm_mssql_database" "db" {
   license_type = "LicenseIncluded"
   max_size_gb  = 2
   sku_name     = "Basic"
-}
-
-resource "azurerm_mssql_firewall_rule" "azure_firewall_exception" {
-  name             = "AllowAccessFromAzure"
-  server_id        = azurerm_mssql_server.sql_server.id
-  start_ip_address = "0.0.0.0"
-  end_ip_address   = "0.0.0.0"
-
-  depends_on = [azurerm_mssql_database.db]
-}
-
-resource "null_resource" "create-sql-user" {
-
-  provisioner "local-exec" {
-    command = <<EOT
-    Install-Module -Name SqlServer -Force
-     Invoke-Sqlcmd -Query "CREATE USER [${azurerm_linux_web_app.pizza_selector_be.name}] FROM EXTERNAL PROVIDER; ALTER ROLE db_datareader ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}]; ALTER ROLE db_datawriter ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}]; ALTER ROLE db_ddladmin ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}];" -ConnectionString "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name},1433;Initial Catalog=Master;User Id=terraformoperator;Password=${random_password.password.result};Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;" 
-    EOT
-
-    interpreter = ["pwsh", "-Command"]
-  }
-
-  depends_on = [azurerm_mssql_database.db]
 }
---
+++ infrastructure/sqlscripts/create_user_dev.sql
@@ -0,0 +1,8 @@
+IF NOT EXISTS(SELECT principal_id FROM sys.database_principals WHERE name = 'pizza-selector-be-dev') BEGIN
+    CREATE USER [pizza-selector-be-dev] FROM EXTERNAL PROVIDER; 
+
+    ALTER ROLE db_datareader ADD MEMBER [pizza-selector-be-dev]; 
+    ALTER ROLE db_datawriter ADD MEMBER [pizza-selector-be-dev]; 
+    ALTER ROLE db_ddladmin ADD MEMBER [pizza-selector-be-dev];
+END
+
