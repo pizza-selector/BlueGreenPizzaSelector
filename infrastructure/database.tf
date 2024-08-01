42de65150875cb8ae1d1c4211802100d2fad21a2
Mikhail Koptelov <mkoptelov@gmail.com>
2024-08-01 20:37:36 +0200

    admin name change?

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -9,11 +9,11 @@ resource "azurerm_mssql_server" "sql_server" {
   resource_group_name          = azurerm_resource_group.pizza_selector_rg.name
   location                     = azurerm_resource_group.pizza_selector_rg.location
   version                      = "12.0"
-  administrator_login          = "terraform_administrator_login"
+  administrator_login          = "tfadmin"
   administrator_login_password = random_password.password.result
+  minimum_tls_version          = "1.2"
 
   azuread_administrator {
-    azuread_authentication_only = false
     login_username              = "Pizza Selector"
     object_id                   = "b05c30c9-c432-4c8d-ae49-1f4528f6ccdc"
   }
@@ -42,7 +42,7 @@ resource "null_resource" "create-sql-user" {
   provisioner "local-exec" {
     command = <<EOT
     Install-Module -Name SqlServer -Force
-     Invoke-Sqlcmd -Username terraform_administrator_login -Password ${random_password.password.result} -Query "CREATE USER [${azurerm_linux_web_app.pizza_selector_be.name}] FROM EXTERNAL PROVIDER; ALTER ROLE db_datareader ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}]; ALTER ROLE db_datawriter ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}]; ALTER ROLE db_ddladmin ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}];" -ConnectionString "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name}.database.windows.net,1433;Initial Catalog=Master;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;" 
+     Invoke-Sqlcmd -Username tfadmin -Password ${random_password.password.result} -Query "CREATE USER [${azurerm_linux_web_app.pizza_selector_be.name}] FROM EXTERNAL PROVIDER; ALTER ROLE db_datareader ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}]; ALTER ROLE db_datawriter ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}]; ALTER ROLE db_ddladmin ADD MEMBER [${azurerm_linux_web_app.pizza_selector_be.name}];" -ConnectionString "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name}.database.windows.net,1433;Initial Catalog=Master;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;" 
     EOT
 
     interpreter = ["pwsh", "-Command"]
