161c5e0f81c45a25a883e94ddbcab3464a6a4c20
Mikhail Koptelov <mkoptelov@gmail.com>
2024-08-01 22:09:39 +0200

    fmt as usual

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -1,15 +1,15 @@
 
 resource "azurerm_mssql_server" "sql_server" {
-  name                         = "pizza-sqlserver-${var.env}"
-  resource_group_name          = azurerm_resource_group.pizza_selector_rg.name
-  location                     = azurerm_resource_group.pizza_selector_rg.location
-  version                      = "12.0"
-  minimum_tls_version          = "1.2"
+  name                = "pizza-sqlserver-${var.env}"
+  resource_group_name = azurerm_resource_group.pizza_selector_rg.name
+  location            = azurerm_resource_group.pizza_selector_rg.location
+  version             = "12.0"
+  minimum_tls_version = "1.2"
 
   azuread_administrator {
     azuread_authentication_only = true
-    login_username = "Pizza Selector"
-    object_id      = "dd8e1ef7-e75d-4373-868a-1f331442ba81"
+    login_username              = "Pizza Selector"
+    object_id                   = "dd8e1ef7-e75d-4373-868a-1f331442ba81"
   }
 }
 
